83c42350c0c6bbb4130125a5eb9f296ddd536daa
CARLOS TRIGO <carlos@kryha.io>
2024-06-13 07:09:41 +0200

    set up tests

---
+++ src/db/mock-data.ts
@@ -0,0 +1,26 @@
+export const mockTodos = [
+  {
+    id: 1,
+    name: "make lasagna",
+    created_on: "2024-06-11T22:07:36.373Z",
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "eat lasagna",
+    created_on: "2024-06-11T23:30:44.263Z",
+    completed: false,
+  },
+  {
+    id: 6,
+    name: "write tests",
+    created_on: "2024-06-12T00:24:18.333Z",
+    completed: true,
+  },
+  {
+    id: 7,
+    name: "walk the dog",
+    created_on: "2024-06-12T09:05:20.294Z",
+    completed: true,
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import express from "express";
 import "express-async-errors";
 import http from "http";
